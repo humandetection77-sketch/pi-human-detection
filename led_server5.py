ff419f5a5cfa839fed086817aec2c856268ee4f5
humandetection77-sketch <humandetection77@gmail.com>
2025-11-23 16:39:03 -0500

    Update and rename led_server4.py to led_server5.py

--- led_server5.py
+++ led_server5.py
@@ -1,17 +1,23 @@
-from gpiozero import LED
+from gpiozero import LED, Device
+from gpiozero.pins.lgpio import LGPIOFactory
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# Use LGPIOFactory so gpiozero works on Pi 5 64-bit
+Device.pin_factory = LGPIOFactory()
+
 LED_PIN = 18
-led = LED(LED_PIN)  # gpiozero handles SOC access correctly on Pi 5
+led = LED(LED_PIN)
 
 class LEDHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/on":
             led.on()
             self.respond(b"LED ON")
+            print("LED ON")
         elif self.path == "/off":
             led.off()
             self.respond(b"LED OFF")
+            print("LED OFF")
         else:
             self.respond(b"Unknown command")
 
